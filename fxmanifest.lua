3be7de25f3ae8983b50eed507b63e7c91125e3ff
Tomek <88977612+Mequ0@users.noreply.github.com>
2024-11-06 16:50:05 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author '_Mequ_'
-description 'Skrypt na wpożyczalnie łódek'
+description 'Skrypt na wpożyczalnie łódek https://github.com/Mequ0/mequ_boatrental/'
 
 dependencies {
     'es_extended',
